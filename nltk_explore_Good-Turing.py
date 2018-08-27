f098a1627f11fe083d0e924fd6a436395e264314
Sig Nin <signin@email.com>
2018-08-27 12:22:22 -0700

    Emma trigrams Good-Turing discounting - plot c*/c - using only Nc estimated.

--- nltk_explore_Good-Turing.py
+++ nltk_explore_Good-Turing.py
@@ -29,15 +29,24 @@ show()
 
 # Re-compute N-gram counts for Emma
 c_c_star = []
+c_c_star_est = []
 for count in TG_counts_dist:
     n_c = TG_counts_dist[count]
     n_c_plus_one = TG_counts_dist[count + 1]
     if n_c_plus_one == 0: # there are no trigrams with count c+1
-        log_n_c_plus_one = b + a * (log(count + 1))   # estimate Nc+1
+        log_n_c_plus_one = b + a * log(count + 1)   # estimate Nc+1
         n_c_plus_one = exp(log_n_c_plus_one)
     c_star = (count + 1) * (n_c_plus_one / n_c)
     c_c_star += [ ( count, c_star, ) ]
+    # Recompute using estimated Nc and Nc+1
+    log_n_c_est = b + a * log(count)                # estimate Nc
+    n_c_est = exp(log_n_c_est)
+    log_n_c_plus_one_est = b + a * log(count + 1)   # estimate Nc+1
+    n_c_plus_one_est = exp(log_n_c_plus_one_est)
+    c_star_est = (count + 1) * (n_c_plus_one_est / n_c_est)
+    c_c_star_est += [ ( count, c_star_est, ) ]
 TG_counts_dist_GT = dict(c_c_star)
+TG_counts_dist_GT_ext = dict(c_c_star_est)
 TG_dist_GT = { gram : TG_counts_dist_GT[TG_dist[gram]] for gram in TG_dist}
 
 # Compute some validations ...
@@ -64,6 +73,38 @@ title("Emma - trigram counts vs. Good-Turing discounted counts, first 100")
 ylabel("c*")
 xlabel("c")
 show()
+
+# Plot c vs c*/c
+c_cs_c_ratio_sorted = [ (c, TG_counts_dist_GT[c]/c,) for c in c_sorted ]
+x, y = zip(*c_cs_c_ratio_sorted)
+plot(x[:100],y[:100])
+plot(x[:100],y[:100],'o')
+title("Emma - trigram count Good-Turing discounted ratios, first 100")
+ylabel("c*/c")
+xlabel("c")
+show()
+
+# Plot c vs. c* using estimated Nc and Nc+1
+c_cs_est_sorted = [ (c, TG_counts_dist_GT_ext[c],) for c in c_sorted ]
+x, y = zip(*c_cs_est_sorted)
+plot(x[:100],y[:100])
+c_max = c_sorted[99]
+plot(range(c_max),range(c_max),"r")
+title("Emma - trigram counts vs. Good-Turing discounted counts, est Nc, first 100")
+ylabel("c*")
+xlabel("c")
+show()
+
+# Plot c vs c*/c using estimated Nc and Nc+1
+c_cs_est_c_ratio_sorted = [ (c, TG_counts_dist_GT_ext[c]/c,) for c in c_sorted ]
+x, y = zip(*c_cs_est_c_ratio_sorted)
+plot(x[:100],y[:100])
+plot(x[:100],y[:100],'o')
+title("Emma - trigram count Good-Turing discounted estimated ratios, first 100")
+ylabel("c*/c")
+xlabel("c")
+show()
+
 
 # List Trigram counts for Emma
 outFileName = "Emma_trigrams_GT_List.txt"
