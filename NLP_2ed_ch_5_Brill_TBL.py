109fe77e4fae1d1b6b4e61faae59586bb4898ab0
Sig Nin <signin@email.com>
2018-09-07 11:55:00 -0700

    NLP 2ed problems 5.5, 5.6 solutions - update - corrected typos in Brill TBL - added some basic tests - test_transform_m1() is for the default template - include an entry for the start tag in the   counts of good and bad transforms

--- NLP_2ed_ch_5_Brill_TBL.py
+++ NLP_2ed_ch_5_Brill_TBL.py
@@ -6,21 +6,33 @@ Algorithm sketched in NLP 2nd Ed figure 5.21.
 Sig Nin
 5 Sep 2018
 """
+
+# Each template is [ type, tagFrom, tagTo, tagZ, tagW, description ]
+# where the description is a format string with places for the tags used.
+# The tagger is initialized with the simple template used in the
+# algorithm sketch in NLP 2ed figure 5.21.
+# The NIL template is the starting point for the best transform.
+# A template requires corresponding code in the tagger.
+DEFAULT_TEMPLATE = [
+        'm1', '', '', '', '', "Change %s to %s if preceding tag is %s%s."
+    ]
+NIL_TEMPLATE = [
+        'NIL', '', '', '', '', "Don't change the tag%s%s%s%s."
+    ]
+
+# Start of text tag, for positions preceding the beginning of the text.
+START_TAG = '<START>'
+
 class BrillTBL:
-
-    # Each template is [ type, tagFrom, tagTo, tagZ, tagW, description ]
-    # where the description is a format string with places for the tags used.
-    # The tagger is initialized with the simple template used in the
-    # algorithm sketch in NLP 2ed figure 5.21.
-    # The NIL template is the starting point for the best transform.
-    # A template requires corresponding code in the tagger.
-    DEFAULT_TEMPLATE =
-        [ 'm1', '', '', '', '', "Change %s to %s if preceding tag is %s%s." ]
-    NIL_TEMPLATE =
-        [ 'NIL', '', '', '', '', "Don't change the tag%s%s%s%s." ]
-
-    # Start of text tag, for positions preceding the beginning of the text.
-    START_TAG = '<START>'
+    """
+    Brill TBL tagger.
+    Starting with an initial tag and rule templates of the form
+        "Change <fromTag> to <toTag> iff <tagZ, tagW in context>"
+    learns a series of rules in which the tags are resolved
+    that improve the tagging of the training set compared to a
+    ground truth tagging.
+    The series of rules can be applied to another corpus to tag it.
+    """
 
     # This is the constructor, which takes a corpus with an initial tagging,
     # a reference ground-truth tagging, a list of possible tags - the tagset,
@@ -45,7 +57,7 @@ class BrillTBL:
         self.tagged_words_true = tagged_words_true
         self.tagset = tagset
         self.name = name
-        self.templates = DEFAULT_TEMPLATE
+        self.templates = [ DEFAULT_TEMPLATE ]
         self.transforms_queue = []
 
     # Display a transform instance
@@ -62,6 +74,8 @@ class BrillTBL:
         Get the best transform sequence for this initial tagging.
         Return a list of the best rules, transform instances
         with the tag slots set.
+        The learned rules can be applied with the "tag()" method
+        to another corpus.
         """
         minimum_change = 10
         last_score = 0
@@ -91,7 +105,7 @@ class BrillTBL:
         return best_transform
 
     # Test transform at every position across the corpus
-    def test_transform(template, fromTag, toTag,
+    def test_transform_m1(self, fromTag, toTag,
             count_good_transforms, count_bad_transforms):
         """
         Tests the effect of the transform with the given tags.
@@ -104,7 +118,7 @@ class BrillTBL:
             current_tag = self.tagged_words[pos][1]
             # Apply the default template criteria
             if pos > 0:
-                preceding_tag = self.tagged_words[pos-1][]
+                preceding_tag = self.tagged_words[pos-1][1]
             else:
                 preceding_tag = START_TAG
             if correct_tag == toTag and current_tag == fromTag:
@@ -114,7 +128,7 @@ class BrillTBL:
 
 
     # Get the best instance of a transform.
-    def get_best_instance(template):
+    def get_best_instance(self, template):
         """
         The best transform is an instance of a template
             [ type fromTag toTag zTag wTag description ]
@@ -127,11 +141,15 @@ class BrillTBL:
         """
         best_transform = ( NIL_TEMPLATE, 0, )
         # counts of good and bad results for a transform instance
-        count_good_transforms = { tags : 0 for tag in self.tagset }
-        count_bad_transforms  = { tags : 0 for tag in self.tagset }
+        count_good_transforms = { (tag, '') : 0 for tag in self.tagset }
+        count_good_transforms[(START_TAG, '')] = 0
+        count_bad_transforms  = { (tag, '') : 0 for tag in self.tagset }
+        count_bad_transforms[(START_TAG, '')] = 0
+        # iterate over all tag pairs ...
         for fromTag in self.tagset:
             for toTag in self.tagset:
-                test_transform(template, fromTag, toTag,
+                # test the default transform, context just preceding tag
+                test_transform_m1(fromTag, toTag,
                         count_good_transforms, count_bad_transforms)
                 # Find the best tags z and w among those encountered,
                 # the one that produced the most improvements in the
@@ -139,9 +157,9 @@ class BrillTBL:
                 bestZ = START_TAG
                 bestScore = 0
                 for zTag in self.tagset: # for now, just iterate over z
-                    score = count_good_transforms[(zTag, '',)]
+                    score = count_good_transforms[(zTag, '',)] \
                            - count_bad_transforms[(zTag, '',)]
-                    if score > bestScore
+                    if score > bestScore:
                         bestScore = score
                         bestZ = zTag
                 if bestScore > best_transform[1]:
@@ -162,7 +180,55 @@ class BrillTBL:
         previous_tag = START_TAG
         for pos in range(len(self.tagged_words)):
             current_tag = self.tagged_words[pos][1]
-            if pos > 0
+            if pos > 0:
                 previous_tag = self.tagged_words[pos-1][1]
             if current_tag == tagFrom and previous_tag == zTag:
                 self.tagged_words[pos][1] = toTag
+
+# ------------------------------------------------------------------------
+# Tests ---
+# ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    import nltk
+    from nltk.corpus import treebank as tb
+    from NLP_2ed_ch_5_problems import split_train_test
+    from random import random as rand
+
+    # NLTK treebank corpus tests.
+    tb_training_sents, tb_test_sents = split_train_test(tb.tagged_sents())
+    tb_tagged_words = [ (w, tag, ) for s in tb.tagged_sents()
+                                   for w, tag in s]
+    tb_training_tagged_words = [ (w, tag, ) for s in tb_training_sents
+                                            for w, tag in s]
+    tb_test_tagged_words = [ (w, tag, ) for s in tb_test_sents
+                                        for w, tag in s]
+    tb_tagset = set(tag for word, tag in tb_tagged_words)
+    tb_training_tagged_words_NN = [ (w, 'NN',)
+                                    for w, tag in tb_training_tagged_words]
+    tb_test_tagged_words_NN = [ (w, 'NN',)
+                                for w, tag in tb_test_tagged_words]
+
+    # Test class BrillTBL
+    tagger = BrillTBL(tb_training_tagged_words_NN,
+                      tb_training_tagged_words,
+                      tb_tagset,
+                      "NLTK Penn-Treebank initially tagged NN")
+
+    # Test test_transform_m1()
+    count_good_transforms = { (tag, '') : 0 for tag in tagger.tagset }
+    count_good_transforms[(START_TAG, '')] = 0
+    count_bad_transforms  = { (tag, '') : 0 for tag in tagger.tagset }
+    count_bad_transforms[(START_TAG, '')] = 0
+    tagger.test_transform_m1('NN', 'NNP',
+                          count_good_transforms, count_bad_transforms)
+    good_z = [ (k,count_good_transforms[k])
+                for k in count_good_transforms
+                if count_good_transforms[k] > 0 ]
+    
+    bad_z  = [ (k,count_bad_transforms[k])
+                for k in count_bad_transforms
+                if count_bad_transforms[k] > 0 ]
+    print('good z:', good_z)
+    print('bad z:', bad_z)
+    
